15a0eb293238913998a0436fc8a11e9c0e8f1225
juanpprim <jmperezprim@gmail.com>
2025-08-19 23:24:28 +0200

    dummy test

---
+++ demo.py
@@ -0,0 +1,13 @@
+class dummy():
+
+  def __init__(self, ):
+    self.message = None
+
+  def print_m(self, message):
+    print(message)
+
+if __name__ == "__main__":
+
+  print('hello world')
+  m = dummy()
+  m.print_m('hello world')
